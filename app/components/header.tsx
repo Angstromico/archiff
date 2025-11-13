fb24e55c0ebb74a03cf163f31b950ee8e3929ecf
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-11-13 18:49:55 -0400

    feat: Header

---
+++ app/components/header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className='mt-15 lg:mt-20 px-12 lg:px-14'>
+      <h1 className='text-black font-bold text-2xl md:text-4xl lg:text-5xl xl:text-6xl'>
+        Escuela online de <br className='hidden md:block' /> Arquitectura y
+        Diseño.
+      </h1>
+    </header>
+  )
+}
+
+export default Header
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Header from './components/header'
+
 export default function Home() {
   return (
     <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
-      <main className='min-h-screen w-full bg-white'></main>
+      <main className='min-h-screen w-full bg-white'>
+        <Header />
+      </main>
     </div>
   )
 }
