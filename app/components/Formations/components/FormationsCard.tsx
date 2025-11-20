98e3a03ebd1b8e365e4669877ae6b44ec0cafe48
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-11-19 22:03:28 -0400

    feat: Sow bottom info from card on mobile FormationsCard

--- app/components/Formations/components/FormationsCard.tsx
+++ app/components/Formations/components/FormationsCard.tsx
@@ -72,7 +72,7 @@ const FormationsCard = ({
           <p className='serotiva mt-2 lg:mt-3 text-[#2222C2] hidden lg:block text-3xl'>
             {price}€
           </p>
-          <button className='hidden lg:block absolute bottom-6 left-6 border p-1 w-32 h-10 rounded-full text-xl'>
+          <button className='absolute bottom-6 left-6 border p-1 w-28 h-8 lg:w-32 lg:h-10 rounded-full text-base lg:text-xl'>
             + Ver más
           </button>
         </div>
