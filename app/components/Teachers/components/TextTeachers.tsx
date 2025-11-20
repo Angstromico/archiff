61f234807c4ea345dc6372435c1c87ff54bc7d47
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-11-19 21:30:44 -0400

    bugfix: Aligh text on mobile with carrusel in TextTeachers

--- app/components/Teachers/components/TextTeachers.tsx
+++ app/components/Teachers/components/TextTeachers.tsx
@@ -2,7 +2,7 @@ import SectionLogo from '../../SectionLogo'
 
 const TextTeachers = () => {
   return (
-    <div className='flex flex-col gap-3 lg:flex-row lg:justify-between relative'>
+    <div className='lg:flex gap-3 lg:px-0 lg:flex-row lg:justify-between relative'>
       <SectionLogo
         image='/intros/Profesores.svg'
         sizes={{ width: 350, tinyWidth: 209, height: 60 }}
@@ -10,7 +10,7 @@ const TextTeachers = () => {
         noTop
         lessBottom
       />
-      <div className='w-full lg:max-w-1/2 2xl:max-w-[973px] border-2 border-b-0 lg:border-3 lg:border-b-0 px-3 lg:pl-8 lg:pb-0 text-[23px] 2xl:text-[30px]'>
+      <div className='sm:w-full lg:max-w-1/2 2xl:max-w-[973px] border-2 border-b-0 lg:border-3 lg:border-b-0 mx-[30px] sm:mx-0 lg:pl-8 lg:pb-0 text-lg 2xl:text-[30px] text-justify'>
         <p className='w-full max-w-[600px] text-justify md:mt-4 px-1'>
           Equipo docente compuesto por más de 50 profesionales de referencia en
           el sector.
