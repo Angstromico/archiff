5a47ee4b654579f1bf77b1065a289a03fc116268
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-11-24 22:43:16 -0400

    feat: Decrease FadeGradients in mobile

--- app/components/FadeGradients.tsx
+++ app/components/FadeGradients.tsx
@@ -1,8 +1,8 @@
 const FadeGradients = () => {
   return (
     <>
-      <div className='pointer-events-none absolute inset-y-0 left-0 w-32 bg-linear-to-r from-white to-transparent z-10' />
-      <div className='pointer-events-none absolute inset-y-0 right-0 w-32 bg-linear-to-l from-white to-transparent z-10' />
+      <div className='pointer-events-none absolute inset-y-0 left-0 w-16 sm:w-32 bg-linear-to-r from-white to-transparent z-10' />
+      <div className='pointer-events-none absolute inset-y-0 right-0 w-16 sm:w-32 bg-linear-to-l from-white to-transparent z-10' />
     </>
   )
 }
